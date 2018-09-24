739fccce1f4add15d159e658116398b26ee697a7
chenchu <313184923@qq.com>
2018-09-25 00:23:51 +0800

    v002

--- FP.py
+++ FP.py
@@ -1,1 +1,15 @@
 import numpy as ny
+import matplotlib.pyplot as plt
+x=ny.arange(4,-4,0.01)
+y1=x**2
+y2=-x**2
+y3=(x-1)**2
+y4=x**2-1
+
+
+plt.plot(x,y1)
+plt.plot(x,y2)
+plt.plot(x,y3)
+plt.plot(x,y4)
+plt.show()
+
