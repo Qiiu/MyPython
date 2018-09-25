7957f79f262cf7ce0128cff4dd23f7da9b0123d3
chenchu <313184923@qq.com>
2018-09-26 00:17:25 +0800

    v004

--- FP.py
+++ FP.py
@@ -4,8 +4,19 @@ def get_circle_area(r):
   a=r**2*pi
   return a
 
-
 radius=10
 result=get_circle_area(radius)
 
+if radius>5 & radius<15:
+  print("Hellow World!")
+elif radius<5 | radius>10:
+  print("xx")
+
 print(f"the area is {result}")
+
+for i in range(5):
+  print(i)
+  if i==3:
+    break
+else:
+  print("done")
