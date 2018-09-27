3225f10269c1fd38064e71b3ce1183a59b957c58
chenchu <313184923@qq.com>
2018-09-27 22:22:33 +0800

    v007

--- FP.py
+++ FP.py
@@ -1,34 +1,7 @@
-from numpy import pi
+import calendar
+import time
 
-def get_circle_area(r):
-  a=r**2*pi
-  return a
+cal=calendar.month(2018,9)
+print(cal)
 
-radius=10
-result=get_circle_area(radius)
-
-if radius>5 & radius<15:
-  print("Hellow World!")
-elif radius<5 | radius>10:
-  print("xx")
-
-print(f"the area is {result}")
-
-for i in range(5):
-  print(i)
-  if i==3:
-    break
-else:
-  print("done")
-
-if radius==10:
-  pass # pass的意思就是啥也不做，但又不能啥也不写
-else:
-  print("done")
-
-dic={1:"one",2:"two",3:"three"}
-dic[4]="four"
-for item in dic.keys():
-  print(item)
-del dic[2]
-print("value : %s" % dic.values())
+print(time.strftime("%a %b %d %H:%S:%Y",time.localtime()))
