934c5f24c2694a57a41efb814587bcb40dc9f567
chenchu <313184923@qq.com>
2018-09-28 23:35:16 +0800

    v009

--- FP.py
+++ FP.py
@@ -1,23 +1,5 @@
-import calendar
-import time
+# -*- coding: UTF-8 -*-
+# 输入
 
-cal=calendar.month(2018,9)
-print(cal)
-
-print(time.strftime("%a %b %d %H:%S:%Y",time.localtime()))
-
-money=100
-def Add(*value,age,name="cc"):
-# 全局变量的使用
-  global money
-  money=money+1
-  print(money)
-# 有默认值的参数
-  print(f"{name} is {age}")
-# 不定长参数的使用
-  for i in value:
-    print(i)
-
-print(money)
-# 关键词参数的使用
-Add(1,2,3,age=4)
+str=input("可读取python表达式：")
+print(str)
